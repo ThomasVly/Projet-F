77b1be6569d4dee17b97cc0ba5ae9c5ed3e8c9ec
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2024-03-12 12:15:21 +0100

    feat : Home Page

--- lib/classes/accueil.dart
+++ lib/classes/accueil.dart
@@ -13,17 +13,22 @@ class AccueilPage extends StatefulWidget{
 
 class _AccueilPageState extends State<AccueilPage>{
   @override
+  void initState() {
+    super.initState();
+
+    // Utilisez Future.delayed pour attendre 2 secondes avant de naviguer vers une autre page
+    Future.delayed(Duration(seconds: 3), () {
+      // Utilisez Navigator pour naviguer vers une autre page (par exemple, la page suivante)
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) =>const MyHomePage(title:"accueil"), // Remplacez AutrePage par le nom de votre page suivante
+        ),
+      );
+    });
+  }
+  @override
   Widget build(BuildContext context){
      return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Align(
         
         // Center is a layout widget. It takes a single child and positions it
@@ -41,29 +46,42 @@ class _AccueilPageState extends State<AccueilPage>{
             ),
         
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 
                 const SizedBox(
                   width:50,
                 ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    foregroundColor:MaterialStateProperty.all<Color>(Colors.black),
-                    backgroundColor:MaterialStateProperty.all<Color>(Colors.blueGrey),
-                  ),
-                  onPressed: () {
-                     Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Accueil')),
-                    );
-                  },
-                  child: const Text('Connexion'),
+                Image.asset(
+                  'images/logo.png',
+                  width: 200,
+                  height: 200,
                 ),
+                
                 // Vous pouvez ajouter d'autres éléments de la ligne ici si nécessaire
               ],
             ),
-            
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height:200,
+                ),
+                Text(
+                  "Bienvenue jeune padawan",
+                )
+              ]
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'images/gif-diary.gif',
+                  width: 200,
+                  height: 200,
+                ),
+              ]
+            )
           ],
         ),
        
