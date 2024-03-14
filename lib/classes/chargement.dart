b011955bda0aeaf3d9b53ca784ac46bb155df954
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2024-03-14 10:45:08 +0100

    page de chargement responsive

--- lib/classes/chargement.dart
+++ lib/classes/chargement.dart
@@ -15,10 +15,7 @@ class _AccueilPageState extends State<AccueilPage>{
   @override
   void initState() {
     super.initState();
-
-    // Utilisez Future.delayed pour attendre 2 secondes avant de naviguer vers une autre page
     Future.delayed(Duration(seconds: 3), () {
-      // Utilisez Navigator pour naviguer vers une autre page (par exemple, la page suivante)
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (context) =>const MyHomePage(title:"accueil"), // Remplacez AutrePage par le nom de votre page suivante
@@ -28,6 +25,7 @@ class _AccueilPageState extends State<AccueilPage>{
   }
   @override
   Widget build(BuildContext context){
+    Size screenSize = MediaQuery.of(context).size;
      return Scaffold(
       body: Align(
         
@@ -36,39 +34,41 @@ class _AccueilPageState extends State<AccueilPage>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-          Row(
-               mainAxisAlignment: MainAxisAlignment.center,
+          /*const Row(
+               mainAxisAlignment: MainAxisAlignment.center,    // Titre de haut de page
                children: <Widget>[
-              const Text(
+              Text(
                 'Journal Intime',
               ),
                ]
-            ),
+            ),*/
         
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 
-                const SizedBox(
+                /*const SizedBox(
                   width:50,
-                ),
+                ),*/
                 Image.asset(
+                  
                   'images/logo.png',
-                  width: 200,
-                  height: 200,
+                  width : (screenSize.width/2),
+                  height: (screenSize.height/2),
                 ),
                 
                 // Vous pouvez ajouter d'autres éléments de la ligne ici si nécessaire
               ],
             ),
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(
-                  height:200,
-                ),
+                /*SizedBox(
+                  height:50,
+                ),*/
                 Text(
                   "Bienvenue jeune padawan",
+                  style : TextStyle(fontSize: screenSize.width/16)
                 )
               ]
             ),
@@ -77,8 +77,8 @@ class _AccueilPageState extends State<AccueilPage>{
               children: <Widget>[
                 Image.asset(
                   'images/gif-diary.gif',
-                  width: 200,
-                  height: 200,
+                  width : (screenSize.width/4),
+                  height: (screenSize.height/4),
                 ),
               ]
             )
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'classes/myHomePage.dart';
-import 'classes/accueil.dart';
+import 'classes/chargement.dart';
 
 void main() {
   runApp(const MyApp());
