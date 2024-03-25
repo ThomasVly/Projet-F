36dc5a3873c47ac335fea7d4cd7fd0832cee85eb
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2024-03-25 10:47:44 +0100

    TextFields for notes

--- lib/classes/notes.dart
+++ lib/classes/notes.dart
@@ -13,17 +13,21 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
-  late TextEditingController _controller;
+  late TextEditingController _controllertitre;
+  late TextEditingController _controllertexte;
+  DateTime? _selectedDate;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _controllertitre = TextEditingController();
+    _controllertexte = TextEditingController();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controllertitre.dispose();
+    _controllertexte.dispose();
     super.dispose();
   }
 
@@ -34,8 +38,54 @@ class _NotesState extends State<Notes> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
+            TextFormField(
+              readOnly: true,
+              onTap: () async {
+                final DateTime? picked = await showDatePicker(
+                  context: context,
+                  initialDate: _selectedDate ?? DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (picked != null && picked != _selectedDate) {
+                  setState(() {
+                    _selectedDate = picked;
+                  });
+                }
+              },
+              controller: TextEditingController(
+                text: _selectedDate != null
+                    ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                    : '',
+              ),
+              decoration: InputDecoration(
+                hintText: 'Sélectionner une date',
+              ),
+            ),
             TextField(
-              controller: _controller,
+              controller: _controllertitre,
+              decoration: InputDecoration(
+                hintText: 'Titre de la note',
+              ),
+              onSubmitted: (String value) {
+                debugPrint(value);
+              },
+            ),
+            SizedBox(height : 20),
+            TextField(
+              controller: _controllertexte,
+              maxLines: 20,
+              decoration: InputDecoration(
+                hintText: 'Texte de la note',
+                // Vous pouvez utiliser le style pour définir la taille de la police
+                hintStyle: TextStyle(fontSize: 16.0), // Taille de la police
+                // Vous pouvez utiliser contentPadding pour définir le remplissage du contenu
+                contentPadding: EdgeInsets.all(10.0), // Remplissage du contenu
+                // Vous pouvez utiliser border pour définir le style de la bordure
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0), // Rayon de la bordure
+                ),
+              ),
               onSubmitted: (String value) {
                 debugPrint(value);
               },
